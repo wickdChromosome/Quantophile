38eb026ae5bf6d0afb7b085cd24de36fa77ca14a
wickd Chromosome <capsule.edu@gmail.com>
2017-05-23 06:53:34 -0400

    Create m_stock_input.h

---
+++ m_stock_input.h
@@ -0,0 +1,30 @@
+/*
+Manually enter stocks, then feed into dlcl function.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int m_stock_input(){
+    
+    //declare vars
+    string index;
+    
+    
+    //make menu for stock name entry
+    do {
+        cout << "Please enter exact stock or option index" << endl;
+        cin >> index;
+        //open yahoo indexes file for reading
+        indexes_file.open("yahoo_indexes.txt");
+        
+        
+        
+    }while index !=
+    
+    
+    
+    
+    
+}
